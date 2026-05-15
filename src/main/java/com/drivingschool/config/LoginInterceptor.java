29f44e879c783575b50cf76ee3b88014825b5351
IT25100878 <it25100878@my.sliit.lk>
2026-05-16 01:15:46 +0530

    Add servlet and interceptor imports for session handling

--- src/main/java/com/drivingschool/config/LoginInterceptor.java
+++ src/main/java/com/drivingschool/config/LoginInterceptor.java
@@ -1,4 +1,9 @@
 package com.drivingschool.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
 public class LoginInterceptor {
 }
