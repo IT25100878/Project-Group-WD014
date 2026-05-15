9a2e716910e3544c3cd73076e55ac7f740896daa
IT25103961 <it25103961@my.sliit.lk>
2026-05-15 21:23:07 +0530

    Add attributes to Admin Class

--- src/main/java/com/drivingschool/model/Admin.java
+++ src/main/java/com/drivingschool/model/Admin.java
@@ -1,6 +1,10 @@
 package com.drivingschool.model;
 
 public class Admin {
+    private String id;
+    private String username;
+    private String password;
+    private String role; // SuperAdmin, Admin
 
 
 }
