418dd5d15ec425ab0a77e57c328a8e8c18e4af4d
IT25102335 <it25102335@my.sliit.lk>
2026-05-16 05:10:08 -0500

    I added vehicle attributes and constructor

--- src/main/java/com/drivingschool/model/Vehicle.java
+++ src/main/java/com/drivingschool/model/Vehicle.java
@@ -2,5 +2,20 @@ package com.drivingschool.model;
 
 public class Vehicle {
 
+    private String id;
+    private String regNo;      // Registration number
+    private String model;
+    private String type;       // Car, Motorcycle
+    private String fuelType;   // Petrol, Diesel, Electric
+    private String status;     // Available, Maintenance, Assigned
+
+    public Vehicle(String id, String regNo, String model, String type, String fuelType, String status) {
+        this.id = id;
+        this.regNo = regNo;
+        this.model = model;
+        this.type = type;
+        this.fuelType = fuelType;
+        this.status = status;
+    }
 
 }
