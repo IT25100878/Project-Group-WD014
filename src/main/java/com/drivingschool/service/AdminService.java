076e487a7a7510f1cf513206225af4d1e26c66fa
IT25103961 <it25103961@my.sliit.lk>
2026-05-16 10:27:02 +0530

    Implemented addAdmin method in AdminService

--- src/main/java/com/drivingschool/service/AdminService.java
+++ src/main/java/com/drivingschool/service/AdminService.java
@@ -1,4 +1,25 @@
 package com.drivingschool.service;
 
+import com.drivingschool.model.Admin;
+import com.drivingschool.util.FileHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
 public class AdminService {
+    @Autowired private FileHandler fileHandler;
+    private final String FILE_NAME = "admins.txt";
+
+    public void addAdmin(Admin admin) throws IOException {
+        fileHandler.appendLine(FILE_NAME, admin.toFileString());
+    }
+
+
+
+
 }
