2664f275df4c5c748895483c4dd61b6dc8aa9a85
IT25100878 <it25100878@my.sliit.lk>
2026-05-16 11:45:49 +0530

    Add login interceptor for admin authentication

--- src/main/java/com/drivingschool/config/LoginInterceptor.java
+++ src/main/java/com/drivingschool/config/LoginInterceptor.java
@@ -5,5 +5,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public class LoginInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("loggedAdmin") == null) {
+            response.sendRedirect("/login");
+            return false;
+        }
+        return true;
+    }
 }
