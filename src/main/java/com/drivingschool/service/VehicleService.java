129df38da84d11d8f9a93199e3e939d2c9d5ccec
IT25102335 <it25102335@my.sliit.lk>
2026-05-16 13:45:42 -0500

    Start VehicleService implementation

--- src/main/java/com/drivingschool/service/VehicleService.java
+++ src/main/java/com/drivingschool/service/VehicleService.java
@@ -1,6 +1,15 @@
 package com.drivingschool.service;
 
+import com.drivingschool.model.Vehicle;
+import com.drivingschool.util.FileHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class VehicleService {
 
     @Autowired
