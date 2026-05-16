fdeadc223d7a4dacee7c59594391619e0f22a8ed
IT25102335 <it25102335@my.sliit.lk>
2026-05-16 05:35:06 -0500

    Added fromFileString() method and implemented it

--- src/main/java/com/drivingschool/model/Vehicle.java
+++ src/main/java/com/drivingschool/model/Vehicle.java
@@ -36,4 +36,9 @@ public class Vehicle {
         return id + "|" + regNo + "|" + model + "|" + type + "|" + fuelType + "|" + status;
     }
 
+    public static Vehicle fromFileString(String line) {
+        String[] parts = line.split("\\|");
+        return new Vehicle(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]);
+    }
+
 }
