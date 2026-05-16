bb6d55c8ac9a667db773fbc821d79d69fc2c949a
IT25102335 <it25102335@my.sliit.lk>
2026-05-16 11:28:36 -0500

    Added required imports for vehicleController

--- src/main/java/com/drivingschool/controller/VehicleController.java
+++ src/main/java/com/drivingschool/controller/VehicleController.java
@@ -1,5 +1,14 @@
 package com.drivingschool.controller;
 
+import com.drivingschool.model.Vehicle;
+import com.drivingschool.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
 public class VehicleController {
 
 
