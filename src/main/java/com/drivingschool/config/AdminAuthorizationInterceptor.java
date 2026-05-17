aa5edf01f0837e9285eecce78a50a3dfb760c798
IT25103961 <it25103961@my.sliit.lk>
2026-05-17 10:59:23 +0530

    I started creating AdminAuthorizationInterceptor class

---
+++ src/main/java/com/drivingschool/config/AdminAuthorizationInterceptor.java
@@ -0,0 +1,10 @@
+package com.drivingschool.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AdminAuthorizationInterceptor implements HandlerInterceptor{
+
+}
