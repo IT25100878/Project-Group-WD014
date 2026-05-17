c10cae45a27fd91fb914406dafe7bf3efbd2a2bb
IT25102456 <it25102456@my.sliit.lk>
2026-05-17 07:59:46 -0700

    Initialize InstructorController with mappings

--- src/main/java/com/drivingschool/controller/InstructorController.java
+++ src/main/java/com/drivingschool/controller/InstructorController.java
@@ -1,4 +1,15 @@
 package com.drivingschool.controller;
+import com.drivingschool.model.Instructor;
+import com.drivingschool.service.InstructorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+import java.util.List;
 
+@Controller
+@RequestMapping("/instructors")
 public class InstructorController {
+
 }
