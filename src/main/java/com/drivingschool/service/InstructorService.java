bb1d5dded41649d87d1cdaea3d8c9d7ab2ceccd5
IT25102456 <it25102456@my.sliit.lk>
2026-05-17 08:12:15 -0700

    Configure InstructorService class

--- src/main/java/com/drivingschool/service/InstructorService.java
+++ src/main/java/com/drivingschool/service/InstructorService.java
@@ -1,4 +1,14 @@
 package com.drivingschool.service;
+import com.drivingschool.model.Instructor;
+import com.drivingschool.util.FileHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class InstructorService {
+
 }
