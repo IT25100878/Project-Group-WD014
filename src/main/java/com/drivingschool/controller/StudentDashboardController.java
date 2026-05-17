1c1591390b61d8f00a4812a3ad2f2f323c1aa923
it25100188 <it25100188@my.sliit.lk>
2026-05-18 03:04:38 +0530

    Dashboard imports

---
+++ src/main/java/com/drivingschool/controller/StudentDashboardController.java
@@ -0,0 +1,21 @@
+package com.drivingschool.controller;
+
+import com.drivingschool.model.Payment;
+import com.drivingschool.model.Schedule;
+import com.drivingschool.model.Student;
+import com.drivingschool.service.PaymentService;
+import com.drivingschool.service.ScheduleService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StudentDashboardController {
+
+}
