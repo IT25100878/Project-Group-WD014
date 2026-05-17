bb252041b181803f816272f332f2791b10fab274
IT25102456 <it25102456@my.sliit.lk>
2026-05-17 07:51:39 -0700

    Create Instructor class properties

--- src/main/java/com/drivingschool/model/Instructor.java
+++ src/main/java/com/drivingschool/model/Instructor.java
@@ -1,4 +1,14 @@
 package com.drivingschool.model;
 
 public class Instructor {
+    private String licenseNo;
+    private String specialization; // Car, Bike, Both
+    private String status;         // Available, Busy, Inactive
+
+    public Instructor(String id, String name, String phone, String licenseNo, String specialization, String status) {
+        super(id, name, phone);
+        this.licenseNo = licenseNo;
+        this.specialization = specialization;
+        this.status = status;
+    }
 }
